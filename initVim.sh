a0075a35838f63f433e92ae21d98af0f848284cf
carme <tkx264pp@gmail.com>
2012-05-16 01:29:21 +0900

    セットアップスクリプトでvimrc zshrcのシンボリックリンクを作るようにした

--- initVim.sh
+++ initVim.sh
@@ -2,4 +2,6 @@
 
 mkdir -p $HOME/.vim/vim_backup/
 mkdir -p $HOME/.vim/vim_swap/
+ln -s $HOME/.vimrc ./.vimrc
+ln -s $HOME/.zshrc ./.zshrc
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
