cbfbef6da620dc14cb913d417796ad101db598d6
carme <carme@ubuntu-12-vm.(none)>
2012-12-10 19:08:11 +0900

    added gitconfig improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+if [ -e $HOME/.vimrc ]; then
+	mv $HOME/.vimrc $HOME/.vimrc.old.`date "+%y%m%d%H%M%S"`
+fi
+ln -s `pwd`/vimrc $HOME/.vimrc
 
-ln -s `pwd`/.vimrc $HOME/.vimrc 
-ln -s `pwd`/.zshrc $HOME/.zshrc 
+if [ -e $HOME/.zshrc ]; then
+	mv $HOME/.zshrc $HOME/.zshrc.old.`date "+%y%m%d%H%M%S"`
+fi
+ln -s `pwd`/zshrc $HOME/.zshrc
+
+if [ -e $HOME/.gitconfig ]; then
+	mv $HOME/.gitconfig $HOME/.gitconfig.old.`date "+%y%m%d%H%M%S"`
+fi
+ln -s `pwd`/gitconfig $HOME/.gitconfig
